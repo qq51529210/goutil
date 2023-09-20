89f8cea87e6663b2ae5db68c7da7e01144fe6eaa
LinWenBin <51529210@qq.com>
2023-09-20 13:46:25 +0800

    u

--- http.go
+++ http.go
@@ -8,8 +8,19 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"reflect"
 	"time"
 )
+
+// HTTPError 表示 JSON 错误
+type HTTPError struct {
+	Phrase string
+	Detail string
+}
+
+func (e *HTTPError) Error() string {
+	return fmt.Sprintf("phrase: %s, detail: %s", e.Phrase, e.Detail)
+}
 
 // HTTPStatusError 表示状态错误
 type HTTPStatusError int
@@ -18,250 +29,119 @@ func (e HTTPStatusError) Error() string {
 	return fmt.Sprintf("status code %d", e)
 }
 
-// HTTP 封装 http 操作
-// method 方法
-// url 请求地址
-// query 请求参数
-// reqBody 用于读取发送 body
-// resBody 用于解析响应 body 中的 json
-// statusCode 用于判断状态码
-// timeout 超时
-func HTTP[reqData, resData any](method, url string, query url.Values, reqBody *reqData, resBody *resData, statusCode int, timeout time.Duration) error {
-	// 请求
-	var body io.Reader = nil
-	if reqBody != nil {
-		buf := bytes.NewBuffer(nil)
-		json.NewEncoder(buf).Encode(reqBody)
-		body = buf
-	}
-	req, err := http.NewRequest(method, url, body)
-	if err != nil {
-		return err
-	}
-	// 参数
-	if query != nil {
-		req.URL.RawQuery = query.Encode()
-	}
-	// 发送
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	req = req.WithContext(ctx)
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	// 状态码
-	if res.StatusCode != statusCode {
-		return HTTPStatusError(res.StatusCode)
-	}
-	// 解析
-	if resBody != nil {
-		return json.NewDecoder(res.Body).Decode(resBody)
+// HTTPStatusErrorHandle 判断状态码
+func HTTPStatusErrorHandle(res *http.Response, code int) error {
+	if res.StatusCode != code {
+		e := new(HTTPError)
+		err := json.NewDecoder(res.Body).Decode(e)
+		if err != nil {
+			return err
+		}
+		return e
 	}
 	return nil
 }
 
-// HTTPTo 封装 http 操作
-// method 方法
-// url 请求地址
-// query 请求参数
-// reqBody 格式化 json 后写入 body
-// resBody 写入响应的 body 数据
-// statusCode 用于判断状态码
-// timeout 超时
-func HTTPTo[reqData any](method, url string, query url.Values, reqBody *reqData, resBody io.Writer, statusCode int, timeout time.Duration) error {
-	// 请求
+var (
+	// HTTPQueryTag 是 HTTPQuery 解析 tag 的名称
+	HTTPQueryTag = "query"
+)
+
+// HTTPQuery 将结构体 v 格式化到 url.Values
+// 只扫描一层，并略过空值
+func HTTPQuery(v any, q url.Values) url.Values {
+	if q == nil {
+		q = make(url.Values)
+	}
+	rv := reflect.ValueOf(v)
+	vk := rv.Kind()
+	if vk == reflect.Pointer {
+		rv = rv.Elem()
+		vk = rv.Kind()
+	}
+	if vk != reflect.Struct {
+		panic("v must be struct or struct ptr")
+	}
+	return httpQuery(rv, q)
+}
+
+func httpQuery(v reflect.Value, q url.Values) url.Values {
+	vt := v.Type()
+	for i := 0; i < vt.NumField(); i++ {
+		fv := v.Field(i)
+		if !fv.IsValid() {
+			continue
+		}
+		fvk := fv.Kind()
+		if fvk == reflect.Pointer {
+			// 空指针
+			if fv.IsNil() {
+				continue
+			}
+			fv = fv.Elem()
+			fvk = fv.Kind()
+		}
+		// 结构，只一层
+		if fvk == reflect.Struct {
+			continue
+		}
+		if fvk == reflect.String {
+			// 空字符串
+			if fv.IsZero() {
+				continue
+			}
+		}
+		ft := vt.Field(i)
+		tn := ft.Tag.Get(HTTPQueryTag)
+		if tn == "" || tn == "-" {
+			continue
+		}
+		q.Set(tn, fmt.Sprintf("%v", fv.Interface()))
+	}
+	return q
+}
+
+// HTTPClient 封装 http 请求代码
+type HTTPClient[reqData any] struct {
+	// 客户端，需要初始化
+	C *http.Client
+	// 地址，需要初始化
+	URL string
+	// 结果回调，需要初始化
+	OnRes func(*http.Response) error
+}
+
+// DoWithContext 发送请求，query 使用 HTTPQuery 来组成参数
+func (c *HTTPClient[reqData]) Do(method string, query any, reqBody *reqData, timeout time.Duration) error {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	return c.DoWithContext(ctx, method, query, reqBody)
+}
+
+// DoWithContext 发送请求，query 使用 HTTPQuery 来组成参数
+func (c *HTTPClient[reqData]) DoWithContext(ctx context.Context, method string, query any, reqBody *reqData) error {
+	// 请求 body
 	var body io.Reader = nil
 	if reqBody != nil {
 		buf := bytes.NewBuffer(nil)
 		json.NewEncoder(buf).Encode(reqBody)
 		body = buf
 	}
-	req, err := http.NewRequest(method, url, body)
+	// 请求
+	req, err := http.NewRequestWithContext(ctx, method, c.URL, body)
 	if err != nil {
 		return err
 	}
 	// 参数
 	if query != nil {
-		req.URL.RawQuery = query.Encode()
+		req.URL.RawQuery = HTTPQuery(query, req.URL.Query()).Encode()
 	}
 	// 发送
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	req = req.WithContext(ctx)
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
-	// 状态码
-	if res.StatusCode != statusCode {
-		return HTTPStatusError(res.StatusCode)
-	}
-	// 解析
-	if resBody != nil {
-		_, err = io.Copy(resBody, res.Body)
-	}
-	return err
-}
-
-// HTTPFrom 封装 http 操作
-// method 方法
-// url 请求地址
-// query 请求参数
-// reqBody 用于读取发送 body
-// resBody 用于解析响应 body 中的 json
-// statusCode 用于判断状态码
-// timeout 超时
-func HTTPFrom[resData any](method, url string, query url.Values, reqBody io.Reader, resBody *resData, statusCode int, timeout time.Duration) error {
-	// 请求
-	req, err := http.NewRequest(method, url, reqBody)
-	if err != nil {
-		return err
-	}
-	// 参数
-	if query != nil {
-		req.URL.RawQuery = query.Encode()
-	}
-	// 发送
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	req = req.WithContext(ctx)
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	// 状态码
-	if res.StatusCode != statusCode {
-		return HTTPStatusError(res.StatusCode)
-	}
-	// 解析
-	if resBody != nil {
-		return json.NewDecoder(res.Body).Decode(resBody)
-	}
-	return err
-}
-
-// HTTPWithContext 封装 http 操作
-// ctx 超时上下文
-// method 方法
-// url 请求地址
-// query 请求参数
-// reqBody 格式化 json 后写入 body
-// resBody 用于解析响应 body 中的 json
-// statusCode 用于判断状态码
-func HTTPWithContext[reqData, resData any](ctx context.Context, method, url string, query url.Values, reqBody *reqData, resBody *resData, statusCode int) error {
-	// body
-	var body io.Reader = nil
-	if reqBody != nil {
-		buf := bytes.NewBuffer(nil)
-		json.NewEncoder(buf).Encode(reqBody)
-		body = buf
-	}
-	// 请求
-	req, err := http.NewRequest(method, url, body)
-	if err != nil {
-		return err
-	}
-	// query
-	if query != nil {
-		req.URL.RawQuery = query.Encode()
-	}
-	// 发送
-	req = req.WithContext(ctx)
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	// 状态码
-	if res.StatusCode != statusCode {
-		return HTTPStatusError(res.StatusCode)
-	}
-	// 解析
-	if resBody != nil {
-		return json.NewDecoder(res.Body).Decode(resBody)
-	}
-	return nil
-}
-
-// HTTPToWithContext 封装 http 操作
-// ctx 超时上下文
-// method 方法
-// url 请求地址
-// query 请求参数
-// reqBody 格式化 json 后写入 body
-// resBody 写入响应的 body 数据
-// statusCode 用于判断状态码
-func HTTPToWithContext[reqData any](ctx context.Context, method, url string, query url.Values, reqBody *reqData, resBody io.Writer, statusCode int) error {
-	// body
-	var body io.Reader = nil
-	if reqBody != nil {
-		buf := bytes.NewBuffer(nil)
-		json.NewEncoder(buf).Encode(reqBody)
-		body = buf
-	}
-	// 请求
-	req, err := http.NewRequest(method, url, body)
-	if err != nil {
-		return err
-	}
-	// query
-	if query != nil {
-		req.URL.RawQuery = query.Encode()
-	}
-	// 发送
-	req = req.WithContext(ctx)
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	// 状态码
-	if res.StatusCode != statusCode {
-		return HTTPStatusError(res.StatusCode)
-	}
-	// 解析
-	if resBody != nil {
-		_, err = io.Copy(resBody, res.Body)
-	}
-	return err
-}
-
-// HTTPFromWithContext 封装 http 操作
-// ctx 超时上下文
-// method 方法
-// url 请求地址
-// query 请求参数
-// reqBody 用于读取发送 body
-// resBody 用于解析响应 body 中的 json
-// statusCode 用于判断状态码
-func HTTPFromWithContext[resData any](ctx context.Context, method, url string, query url.Values, reqBody io.Reader, resBody *resData, statusCode int) error {
-	// 请求
-	req, err := http.NewRequest(method, url, reqBody)
-	if err != nil {
-		return err
-	}
-	// query
-	if query != nil {
-		req.URL.RawQuery = query.Encode()
-	}
-	// 发送
-	req = req.WithContext(ctx)
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	// 状态码
-	if res.StatusCode != statusCode {
-		return HTTPStatusError(res.StatusCode)
-	}
-	// 解析
-	if resBody != nil {
-		return json.NewDecoder(res.Body).Decode(resBody)
-	}
-	return err
+	// 回调
+	return c.OnRes(res)
 }
