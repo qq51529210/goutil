452981134203b4d86d6e4ee1116f8b8ffb0e8e11
LinWenBin <51529210@qq.com>
2023-11-14 16:27:30 +0800

    u

--- sip/sip.go
+++ sip/sip.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"strings"
+	"sync/atomic"
 )
 
 // 方法
@@ -341,6 +343,164 @@ type Writer interface {
 type Reader interface {
 	ReadLine() (string, error)
 	io.Reader
+}
+
+// reader 用于读取 crlf 的每一行数据
+type reader struct {
+	r io.Reader
+	// 缓存
+	buf []byte
+	// 有效数据起始下标
+	begin int
+	// 有效数据终止下标
+	end int
+	// 已经解析的下标
+	parsed int
+}
+
+// newReader 返回新的 reader
+func newReader(r io.Reader, n int) *reader {
+	if n < 1 {
+		n = MaxMessageLen
+	}
+	return &reader{r: r, buf: make([]byte, n)}
+}
+
+func (r *reader) Raw() []byte {
+	return r.buf[:r.end]
+}
+
+func (r *reader) Reset(reader io.Reader) {
+	r.r = reader
+	r.begin = 0
+	r.end = 0
+	r.parsed = 0
+}
+
+func (r *reader) ReadLine() (string, error) {
+	for {
+		// 尝试查找
+		for r.parsed < r.end {
+			// 找到回车
+			if r.buf[r.parsed] == '\n' {
+				i := r.parsed - 1
+				// 找到换行
+				if r.buf[i] == '\r' {
+					line := string(r.buf[r.begin:i])
+					r.parsed++
+					r.begin = r.parsed
+					r.checkEmpty()
+					return line, nil
+				}
+			}
+			r.parsed++
+		}
+		// 缓存中没有，那么要读取数据了
+		if r.end == len(r.buf) {
+			// 这一行数据太大了，指定的缓存装不下了
+			if r.begin == 0 {
+				return "", errLargeMessage
+			}
+			// 缓存向前移
+			if r.begin > 0 {
+				copy(r.buf, r.buf[r.begin:r.end])
+				r.end -= r.begin
+				r.parsed -= r.begin
+				r.begin = 0
+			}
+		}
+		// 继续读
+		n, err := r.r.Read(r.buf[r.end:])
+		if err != nil {
+			return "", err
+		}
+		r.end += n
+	}
+}
+
+func (r *reader) Read(b []byte) (int, error) {
+	if r.begin == r.end {
+		return r.r.Read(b)
+	}
+	n := copy(b, r.buf[r.begin:r.end])
+	r.begin += n
+	r.parsed += n
+	r.checkEmpty()
+	return n, nil
+}
+
+func (r *reader) checkEmpty() {
+	if r.begin == r.end {
+		r.begin = 0
+		r.parsed = 0
+		r.end = 0
+	}
+}
+
+var (
+	// CSeq 的递增 SN
+	sn32   = int64(0)
+	cseq32 = int64(0)
+)
+
+// GetSN 返回全局递增的 sn
+func GetSN() int64 {
+	return atomic.AddInt64(&sn32, 1)
+}
+
+// GetCSeq 返回全局递增的 sn
+func GetCSeq() int64 {
+	return atomic.AddInt64(&cseq32, 1)
+}
+
+// GetSNString 返回字符串形式的全局递增的 sn
+func GetSNString() string {
+	return fmt.Sprintf("%d", atomic.AddInt64(&sn32, 1))
+}
+
+// TrimByte 去掉两端的字符
+func TrimByte(str string, left, right byte) string {
+	if str == "" {
+		return str
+	}
+	i1 := 0
+	for ; i1 < len(str); i1++ {
+		if str[i1] != left {
+			break
+		}
+	}
+	i2 := len(str) - 1
+	for ; i2 > i1; i2-- {
+		if str[i2] != right {
+			break
+		}
+	}
+	return str[i1 : i2+1]
+}
+
+// TrimQuotationMark TrimByte(s, '"', '"')
+func TrimQuotationMark(s string) string {
+	return TrimByte(s, '"', '"')
+}
+
+// 字符
+const (
+	CharComma     = ','
+	CharSpace     = ' '
+	CharEqual     = '='
+	CharSemicolon = ';'
+	CharAmpersand = '&'
+	CharColon     = ':'
+	CharAt        = '@'
+)
+
+// Split 根据第一个 c 切分 s
+func Split(s string, c byte) (prefix, suffix string) {
+	i := strings.IndexByte(s, c)
+	if i < 0 {
+		return s, ""
+	}
+	return s[:i], s[i+1:]
 }
 
 // ResponseError 表示 sip 响应消息的错误
