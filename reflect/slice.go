4225ebb4d6a0592edbb841704ffd63b92670ae01
LinWenBin <51529210@qq.com>
2023-11-17 16:26:05 +0800

    u

--- reflect/slice.go
+++ reflect/slice.go
@@ -89,3 +89,53 @@ func structToSlice(v reflect.Value, m []any) []any {
 	}
 	return m
 }
+
+var (
+	// StructFieldNameSliceTagName 是 StructFieldNameSlice 结构解析的 tag 名称
+	StructFieldNameSliceTagName = "field"
+)
+
+// StructFieldNameSlice 按顺序提取所有字段名称
+func StructFieldNameSlice(v any) []string {
+	vv := reflect.ValueOf(v)
+	if !vv.IsValid() {
+		panic("vv is invalid")
+	}
+	if vv.Kind() == reflect.Pointer {
+		vv = vv.Elem()
+	}
+	if vv.Kind() != reflect.Struct {
+		panic("v must be struct")
+	}
+	return structFieldNameSlice(vv.Type(), make([]string, 0))
+}
+
+// structFieldNameSlice  是 StructFieldNameSlice 的实现
+func structFieldNameSlice(st reflect.Type, a []string) []string {
+	// 所有字段
+	for i := 0; i < st.NumField(); i++ {
+		// 类型
+		ft := st.Field(i)
+		// tag
+		name, _, ignore := parseTag(&ft, StructFieldNameSliceTagName)
+		// 忽略 / 不可导出
+		if ignore || !ft.IsExported() {
+			continue
+		}
+		// 嵌入
+		if ft.Anonymous {
+			t := ft.Type
+			if ft.Type.Kind() == reflect.Pointer {
+				t = t.Elem()
+			}
+			// 结构
+			if t.Kind() == reflect.Struct {
+				a = structFieldNameSlice(t, a)
+			}
+			continue
+		}
+		// 其他
+		a = append(a, name)
+	}
+	return a
+}
