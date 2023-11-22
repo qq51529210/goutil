100941c517d454bd7ae73cdf9bc4d24478311308
LinWenBin <51529210@qq.com>
2023-11-22 10:11:33 +0800

    u

--- gorm/log.go
+++ gorm/log.go
@@ -9,6 +9,7 @@ import (
 	"gorm.io/gorm/logger"
 )
 
+// Log 用于接收 gorm 日志
 type Log struct {
 	traceID string
 }
@@ -18,22 +19,27 @@ func NewLog(traceID string) logger.Interface {
 	return &Log{traceID: traceID}
 }
 
+// LogMode 实现接口
 func (g *Log) LogMode(logger.LogLevel) logger.Interface {
 	return g
 }
 
+// Info 实现接口
 func (g *Log) Info(ctx context.Context, str string, args ...interface{}) {
 	log.InfoTrace(g.traceID, str)
 }
 
+// Warn 实现接口
 func (g *Log) Warn(ctx context.Context, str string, args ...interface{}) {
 	log.WarnTrace(g.traceID, str)
 }
 
+// Error 实现接口
 func (g *Log) Error(ctx context.Context, str string, args ...interface{}) {
 	log.ErrorTrace(g.traceID, str)
 }
 
+// Trace 实现接口
 func (g *Log) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
 	sql, _ := fc()
 	if err != nil {
