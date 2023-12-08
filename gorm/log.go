bfb6f3f0da7c8e9d9cdbf57c9157f1b80eefd8c2
LinWenBin <51529210@qq.com>
2023-12-08 13:30:11 +0800

    u

--- gorm/log.go
+++ gorm/log.go
@@ -12,11 +12,12 @@ import (
 // Log 用于接收 gorm 日志
 type Log struct {
 	traceID string
+	lg      *log.Logger
 }
 
 // NewLog 用于接收  的日志
-func NewLog(traceID string) logger.Interface {
-	return &Log{traceID: traceID}
+func NewLog(traceID string, lg *log.Logger) logger.Interface {
+	return &Log{traceID: traceID, lg: lg}
 }
 
 // LogMode 实现接口
@@ -26,27 +27,27 @@ func (g *Log) LogMode(logger.LogLevel) logger.Interface {
 
 // Info 实现接口
 func (g *Log) Info(ctx context.Context, str string, args ...interface{}) {
-	log.InfoTrace(g.traceID, str)
+	g.lg.InfoTrace(g.traceID, str)
 }
 
 // Warn 实现接口
 func (g *Log) Warn(ctx context.Context, str string, args ...interface{}) {
-	log.WarnTrace(g.traceID, str)
+	g.lg.WarnTrace(g.traceID, str)
 }
 
 // Error 实现接口
 func (g *Log) Error(ctx context.Context, str string, args ...interface{}) {
-	log.ErrorTrace(g.traceID, str)
+	g.lg.ErrorTrace(g.traceID, str)
 }
 
 // Trace 实现接口
 func (g *Log) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
 	sql, _ := fc()
 	if err != nil {
+		g.lg.DebugfTrace(g.traceID, "[%v] %s", time.Since(begin), sql)
 		if err != gorm.ErrRecordNotFound {
-			log.ErrorfTrace(g.traceID, "[%v] %s %v", time.Since(begin), sql, err)
+			g.lg.ErrorTrace(g.traceID, err)
 			return
 		}
 	}
-	log.DebugfTrace(g.traceID, "[%v] %s", time.Since(begin), sql)
 }
