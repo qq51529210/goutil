f7a1440f0860c7b1411d4eaa987d1ccc9b28d757
LinWenBin <51529210@qq.com>
2023-12-08 13:56:42 +0800

    u

--- cfg.go
+++ cfg.go
@@ -9,8 +9,6 @@ import (
 	"path/filepath"
 	"strings"
 
-	"goutil/log"
-
 	"gopkg.in/yaml.v3"
 )
 
@@ -93,36 +91,4 @@ func ReadYAMLCfg(path string, ptr any) error {
 	defer file.Close()
 	// 解析
 	return yaml.NewDecoder(file).Decode(ptr)
-}
-
-// LogCfg 日志的配置
-type LogCfg struct {
-	log.FileConfig `yaml:",inline"`
-	// 日志头格式
-	HeaderFormat string `json:"headerFormat" yaml:"headerFormat" validate:"omitempty,oneof=default fileName filePath"`
-	// 禁用的日志级别
-	DisableLevel []string `json:"disableLevel" yaml:"disableLevel" validate:"omitempty,dive,oneof=all debug info warn error"`
-}
-
-// Init 初始化文件日志
-func (c *LogCfg) Init(name string) error {
-	file, err := log.NewFile(&c.FileConfig)
-	if err != nil {
-		return err
-	}
-	logger := log.NewLogger(file, log.Header(c.HeaderFormat), name, "")
-	logger.DisableLevels(c.DisableLevel)
-	log.SetLogger(logger)
-	//
-	return nil
-}
-
-// SerCfg 服务配置
-type SerCfg struct {
-	// 监听地址
-	Addr string `json:"addr" yaml:"addr" validate:"required,tcp_addr"`
-	// 证书路径
-	CertFile string `json:"certFile" yaml:"certFile" validate:"omitempty,filepath"`
-	// 证书路径
-	KeyFile string `json:"keyFile" yaml:"keyFile" validate:"omitempty,filepath"`
 }
