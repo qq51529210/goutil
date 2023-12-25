c3fee75403c84a9e6160abb496f804b5d648914c
LinWenBin <51529210@qq.com>
2023-12-25 14:52:24 +0800

    u

--- zlm/get_server_config.go
+++ zlm/get_server_config.go
@@ -42,3 +42,26 @@ func (s *Server) GetServerConfig(ctx context.Context) (map[string]string, error)
 	// 没有配置，流媒体服务有问题
 	return nil, ErrConfig
 }
+
+// getServerConfigRes2 是 GetServerConfig 的返回值
+type getServerConfigRes2[M any] struct {
+	apiError
+	Data []M `json:"data"`
+}
+
+// GetServerConfig 调用 /index/api/getServerConfig
+// 它这个返回确实是一个数组，不知道为什么
+func GetServerConfig[M any](ctx context.Context, s *Server) ([]M, error) {
+	// 请求
+	var res getServerConfigRes2[M]
+	err := httpCallRes[any](ctx, s, apiGetServerConfig, nil, &res)
+	if err != nil {
+		return nil, err
+	}
+	if res.apiError.Code != codeTrue {
+		res.apiError.SerID = s.ID
+		res.apiError.Path = apiGetServerConfig
+		return nil, &res.apiError
+	}
+	return res.Data, nil
+}
