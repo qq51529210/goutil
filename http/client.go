57fd889787e4c9dbf57aff9e8e152f503722e466
LinWenBin <51529210@qq.com>
2024-01-02 10:06:08 +0800

    u

--- http/client.go
+++ http/client.go
@@ -181,14 +181,12 @@ type Result[T any] struct {
 	// 状态码
 	Code int `json:"code,omitempty"`
 	// 错误短语
-	Err string `json:"error,omitempty"`
-	// 错误详细
-	Detail string `json:"detail,omitempty"`
+	Msg string `json:"msg,omitempty"`
 	// 没有错误时候的数据
 	Data T `json:"data,omitempty"`
 }
 
 // Error 实现 error 接口
 func (c *Result[T]) Error() string {
-	return fmt.Sprintf("code: %d, error: %s, detail: %s", c.Code, c.Err, c.Detail)
+	return fmt.Sprintf("code: %d, msg: %s", c.Code, c.Msg)
 }
