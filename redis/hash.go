e8e42ab790aba12e51284f35979640d3425591fd
LinWenBin <51529210@qq.com>
2024-01-04 17:37:40 +0800

    u

--- redis/hash.go
+++ redis/hash.go
@@ -7,6 +7,66 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+// HSetNxEx 存在才设置
+func HSetNxEx(ctx context.Context, db redis.UniversalClient, key string, expire time.Duration, field string, value any) (bool, error) {
+	res, err := redis.NewScript(`
+local result = redis.call("EXISTS", KEYS[1])
+-- 存在
+if result == 1 then
+	-- 更新
+	redis.call("HSET", KEYS[1], ARGV[1], ARGV[2])
+	redis.call("EXPIRE", KEYS[1], ARGV[3])
+end
+-- 返回
+return result
+`).Run(ctx, db, []string{key}, field, value, expire/time.Second).Result()
+	if err != nil {
+		return false, err
+	}
+	return res.(int64) == 1, nil
+}
+
+// HSetNx 存在才设置
+func HSetNx(ctx context.Context, db redis.UniversalClient, key, field string, value any) (bool, error) {
+	res, err := redis.NewScript(`
+local result = redis.call("EXISTS", KEYS[1])
+-- 存在
+if result == 1 then
+	-- 更新
+	redis.call("HSET", KEYS[1], ARGV[1], ARGV[2])
+end
+-- 返回
+return result
+`).Run(ctx, db, []string{key}, field, value).Result()
+	if err != nil {
+		return false, err
+	}
+	return res.(int64) == 1, nil
+}
+
+// HMSetNxEx 存在才设置
+func HMSetNxEx(ctx context.Context, db redis.UniversalClient, key string, expire time.Duration, args ...any) (bool, error) {
+	var values []any
+	values = append(values, expire/time.Second)
+	values = append(values, args...)
+	//
+	res, err := redis.NewScript(`
+local result = redis.call("EXISTS", KEYS[1])
+-- 存在
+if result == 1 then
+	-- 更新
+	redis.call("HSET", KEYS[1], ARGV[2], ARGV[3])
+	redis.call("EXPIRE", KEYS[1], ARGV[1])
+end
+-- 返回
+return result
+`).Run(ctx, db, []string{key}, values...).Result()
+	if err != nil {
+		return false, err
+	}
+	return res.(int64) == 1, nil
+}
+
 // HGet 查询哈希，不指定字段则查询全部
 func HGet[M any](ctx context.Context, db redis.UniversalClient, key string, m *M, fields ...string) error {
 	if len(fields) < 1 {
@@ -91,74 +151,55 @@ func HGetAll[M any](ctx context.Context, db redis.UniversalClient, scanKey strin
 // HGetFirst 返回第一个
 func HGetFirst[M any](ctx context.Context, db redis.UniversalClient, scanKey string, m *M, fields ...string) error {
 	// 用迭代器
-	it := db.Scan(ctx, 0, scanKey, 0).Iterator()
+	it := db.Scan(ctx, 0, scanKey, 1).Iterator()
 	for it.Next(ctx) {
-		m := new(M)
-		err := HGet(ctx, db, it.Val(), m, fields...)
-		if err != nil {
-			return err
-		}
-		return nil
+		return HGet(ctx, db, it.Val(), m, fields...)
 	}
 	return it.Err()
 }
 
-// HSetNxEx 存在才设置
-func HSetNxEx(ctx context.Context, db redis.UniversalClient, key string, expire time.Duration, field string, value any) (bool, error) {
-	res, err := redis.NewScript(`
-local result = redis.call("EXISTS", KEYS[1])
--- 存在
-if result == 1 then
-	-- 更新
-	redis.call("HSET", KEYS[1], ARGV[1], ARGV[2])
-	redis.call("EXPIRE", KEYS[1], ARGV[3])
-end
--- 返回
-return result
-`).Run(ctx, db, []string{key}, field, value, expire/time.Second).Result()
+// HGetPageFromSet 使用 set 中的值作为 key 查询
+func HGetPageFromSet[M any](ctx context.Context, db redis.UniversalClient, setKey string, cursor, count uint64, fields ...string) ([]*M, uint64, error) {
+	// 扫描 key
+	cmd := db.SScan(ctx, setKey, cursor, "*", int64(count))
+	keys, _cursor, err := cmd.Result()
 	if err != nil {
-		return false, err
+		return nil, _cursor, err
 	}
-	return res.(int64) == 1, nil
-}
-
-// HSetNx 存在才设置
-func HSetNx(ctx context.Context, db redis.UniversalClient, key, field string, value any) (bool, error) {
-	res, err := redis.NewScript(`
-local result = redis.call("EXISTS", KEYS[1])
--- 存在
-if result == 1 then
-	-- 更新
-	redis.call("HSET", KEYS[1], ARGV[1], ARGV[2])
-end
--- 返回
-return result
-`).Run(ctx, db, []string{key}, field, value).Result()
-	if err != nil {
-		return false, err
+	// 根据 keys 再查询
+	var ms []*M
+	for i := 0; i < len(keys); i++ {
+		m := new(M)
+		if err := HGet(ctx, db, keys[i], m, fields...); err != nil {
+			if err != redis.Nil {
+				return nil, _cursor, err
+			}
+			continue
+		}
+		ms = append(ms, m)
 	}
-	return res.(int64) == 1, nil
-}
-
-// HMSetNxEx 存在才设置
-func HMSetNxEx(ctx context.Context, db redis.UniversalClient, key string, expire time.Duration, args ...any) (bool, error) {
-	var values []any
-	values = append(values, expire/time.Second)
-	values = append(values, args...)
 	//
-	res, err := redis.NewScript(`
-local result = redis.call("EXISTS", KEYS[1])
--- 存在
-if result == 1 then
-	-- 更新
-	redis.call("HSET", KEYS[1], ARGV[2], ARGV[3])
-	redis.call("EXPIRE", KEYS[1], ARGV[1])
-end
--- 返回
-return result
-`).Run(ctx, db, []string{key}, values...).Result()
-	if err != nil {
-		return false, err
+	return ms, _cursor, nil
+}
+
+// HGetAllFromSet 使用 set 中的值作为 key 查询
+func HGetAllFromSet[M any](ctx context.Context, db redis.UniversalClient, setKey string, fields ...string) ([]*M, error) {
+	var ms []*M
+	// 扫描
+	it := db.SScan(ctx, setKey, 0, "*", 0).Iterator()
+	for it.Next(ctx) {
+		m := new(M)
+		if err := HGet(ctx, db, it.Val(), m, fields...); err != nil {
+			if err != redis.Nil {
+				return nil, err
+			}
+			continue
+		}
+		ms = append(ms, m)
 	}
-	return res.(int64) == 1, nil
+	if err := it.Err(); err != nil {
+		return nil, err
+	}
+	//
+	return ms, nil
 }
