219b7c92c7114282be508b324cfb19da6a2ad4f2
LinWenBin <51529210@qq.com>
2024-01-11 13:44:41 +0800

    u

--- gin/forward.go
+++ gin/forward.go
@@ -1,10 +1,10 @@
 package gin
 
 import (
-	"bytes"
-	"encoding/json"
 	"io"
 	"net/http"
+
+	gh "goutil/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -36,73 +36,20 @@ func ForwardWithURL(ctx *gin.Context, client *http.Client, url string) error {
 
 // ForwardResponse 使用参数构造新的请求后转发响应
 func ForwardResponse(ctx *gin.Context, client *http.Client, method, url string, header http.Header, body io.Reader) error {
-	// 请求
-	req, err := http.NewRequestWithContext(ctx, method, url, body)
-	if err != nil {
-		return err
-	}
-	// header
-	for k, v := range header {
-		for _, vv := range v {
-			req.Header.Add(k, vv)
-		}
-	}
-	// 转发
-	return ForwardResponseWithRequest(ctx, client, req)
+	return gh.ForwardResponse(ctx, client, method, url, header, body, ctx.Writer)
 }
 
 // ForwardResponseWithRequest 使用新的请求转发响应
 func ForwardResponseWithRequest(ctx *gin.Context, client *http.Client, req *http.Request) error {
-	// 发送
-	_res, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer _res.Body.Close()
-	// status code
-	ctx.Writer.WriteHeader(_res.StatusCode)
-	// header
-	header := ctx.Writer.Header()
-	for k, v := range _res.Header {
-		for _, vv := range v {
-			header.Add(k, vv)
-		}
-	}
-	// body
-	_, err = io.Copy(ctx.Writer, _res.Body)
-	return err
+	return gh.ForwardResponseWithRequest(ctx, client, req, ctx.Writer)
 }
 
 // ForwardResponseWithReader 使用 reader 作为 body 构造新的请求转发响应
 func ForwardResponseWithReader(ctx *gin.Context, client *http.Client, method, url string, header http.Header, body io.Reader) error {
-	// 请求
-	req, err := http.NewRequestWithContext(ctx, method, url, body)
-	if err != nil {
-		return err
-	}
-	// header
-	for k, v := range header {
-		for _, vv := range v {
-			req.Header.Add(k, vv)
-		}
-	}
-	//
-	return ForwardResponseWithRequest(ctx, client, req)
+	return gh.ForwardResponseWithReader(ctx, client, method, url, header, body, ctx.Writer)
 }
 
 // ForwardResponseWithJSONBody 使用参数构造新的 json body 请求后转发响应
 func ForwardResponseWithJSONBody(ctx *gin.Context, client *http.Client, method, url string, header http.Header, body any) error {
-	// body
-	var data io.ReadWriter
-	if body != nil {
-		data = bytes.NewBuffer(nil)
-		json.NewEncoder(data).Encode(body)
-	}
-	if header == nil {
-		header = make(http.Header)
-	}
-	if header.Get("Content-Type") == "" {
-		header.Add("Content-Type", "application/json; charset=utf-8")
-	}
-	return ForwardResponse(ctx, client, method, url, header, data)
+	return gh.ForwardResponseWithJSONBody(ctx, client, method, url, header, body, ctx.Writer)
 }
--- http/forward.go
+++ http/forward.go
@@ -47,10 +47,49 @@ func ForwardResponse(ctx context.Context, client *http.Client, method, url strin
 	return Forward(client, req, res)
 }
 
-// ForwardResponseWithJSONBody 使用参数构造新的 json body 请求后转发响应
-func ForwardResponseWithJSONBody[Data any](ctx context.Context, client *http.Client, method, url string, header http.Header, body *Data, res http.ResponseWriter) error {
+// ForwardResponseWithRequest 使用新的请求转发响应
+func ForwardResponseWithRequest(ctx context.Context, client *http.Client, req *http.Request, res http.ResponseWriter) error {
+	// 发送
+	_res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer _res.Body.Close()
+	// status code
+	res.WriteHeader(_res.StatusCode)
+	// header
+	header := res.Header()
+	for k, v := range _res.Header {
+		for _, vv := range v {
+			header.Add(k, vv)
+		}
+	}
 	// body
-	var data *bytes.Buffer = nil
+	_, err = io.Copy(res, _res.Body)
+	return err
+}
+
+// ForwardResponseWithReader 使用 reader 作为 body 构造新的请求转发响应
+func ForwardResponseWithReader(ctx context.Context, client *http.Client, method, url string, header http.Header, body io.Reader, res http.ResponseWriter) error {
+	// 请求
+	req, err := http.NewRequestWithContext(ctx, method, url, body)
+	if err != nil {
+		return err
+	}
+	// header
+	for k, v := range header {
+		for _, vv := range v {
+			req.Header.Add(k, vv)
+		}
+	}
+	//
+	return ForwardResponseWithRequest(ctx, client, req, res)
+}
+
+// ForwardResponseWithJSONBody 使用参数构造新的 json body 请求后转发响应
+func ForwardResponseWithJSONBody(ctx context.Context, client *http.Client, method, url string, header http.Header, body any, res http.ResponseWriter) error {
+	// body
+	var data io.ReadWriter
 	if body != nil {
 		data = bytes.NewBuffer(nil)
 		json.NewEncoder(data).Encode(body)
