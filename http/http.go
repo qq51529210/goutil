2ac6b749b585650e41cf5b5ed95279a860f98038
LinWenBin <51529210@qq.com>
2024-01-15 11:39:00 +0800

    u

--- http/http.go
+++ http/http.go
@@ -3,18 +3,20 @@ package http
 import "fmt"
 
 // Result 表示返回的结果
-type Result[T any] struct {
+type Result struct {
 	// 状态码
 	Code int `json:"code,omitempty"`
+	// 正确时返回的数据
+	Data any `json:"data,omitempty"`
 	// 错误短语
 	Msg string `json:"msg,omitempty"`
-	// 没有错误时候的数据
-	Data T `json:"data,omitempty"`
+	// 错误详细
+	Err string `json:"err,omitempty"`
 }
 
 // Error 实现 error 接口
-func (c *Result[T]) Error() string {
-	return fmt.Sprintf("code: %d, msg: %s", c.Code, c.Msg)
+func (c *Result) Error() string {
+	return c.Err
 }
 
 // StatusError 表示状态错误
