fa672b3e8f02555b773a7c160145843b73cb3f61
LinWenBin <51529210@qq.com>
2024-02-05 10:35:57 +0800

    u

--- cache/cache.go
+++ cache/cache.go
@@ -2,240 +2,218 @@ package goutil
 
 import (
 	"context"
-	"sync"
+	"goutil/sync"
 )
 
-type loader struct {
-	c   chan struct{}
-	err error
-}
+// CacheLoadFunc 加载数据操作
+type CacheLoadFunc[K comparable, M any] func(context.Context, *Cache[K, M]) error
 
 // Cache 封装数据库缓存
 type Cache[K comparable, M any] struct {
-	// 同步锁
-	sync.Mutex
-	// 数据
-	D map[K]M
+	D sync.Map[K, M]
+	// 加载，被调用之前，已经加锁了
+	Load CacheLoadFunc[K, M]
 	// 缓存是否有效
 	ok bool
-	// 加载所有数据
-	loadFunc func() (map[K]M, error)
-	// 是否正在加载缓存
-	loading bool
-	// 协程加载
-	loader *loader
 }
 
 // Init 初始化
-func (c *Cache[K, M]) Init(loadFunc func() (map[K]M, error)) {
-	c.D = make(map[K]M)
-	c.loadFunc = loadFunc
+func (c *Cache[K, M]) Init(loadFunc CacheLoadFunc[K, M]) {
+	c.D.Init()
+	c.Load = loadFunc
 }
 
-// Query 首先检查数据是否有效，无效则重新加载
-func (c *Cache[K, M]) Query(ctx context.Context, query func(c *Cache[K, M])) error {
-	c.Lock()
-	// 无效，重新加载所有
-	if !c.ok {
-		var wait *loader
-		// 确保并发下只有一个加载操作
-		if !c.loading {
-			c.loading = true
-			c.loader = new(loader)
-			c.loader.c = make(chan struct{})
-			go c.loadAllRoutine()
-		}
-		wait = c.loader
-		c.Unlock()
-		// 等待结果
-		select {
-		case <-ctx.Done():
-			// 上下文结果
-			return ctx.Err()
-		case <-wait.c:
-			// 加载失败，返回
-			if wait.err != nil {
-				return wait.err
-			}
-			// 加载成功，往下
-		}
-	}
-	// 有效，查询
-	query(c)
-	c.Unlock()
-	return nil
-}
-
-// loadAllRoutine 在协程中加载数据
-func (c *Cache[K, M]) loadAllRoutine() {
-	data, err := c.loadFunc()
-	c.Lock()
-	c.loader.err = err
-	if err == nil {
-		c.D = data
-		c.ok = true
-	}
-	c.loading = false
-	// 通知
-	close(c.loader.c)
-	c.Unlock()
-}
-
-// Invalidate 设置数据无效，这会导致下一次操作重新加载数据
-func (c *Cache[K, M]) Invalidate() {
-	c.Lock()
+// Invalid 设置数据无效
+func (c *Cache[K, M]) Invalid() {
+	c.D.Lock()
 	c.ok = false
-	c.D = make(map[K]M)
-	c.Unlock()
+	c.D.Unlock()
 }
 
-// Add 添加
-func (c *Cache[K, M]) Add(k K, m M) {
-	c.Lock()
-	c.D[k] = m
-	c.Unlock()
-}
-
-// Update 更新，f 回调的 m 可能为零值
-func (c *Cache[K, M]) Update(k K, f func(m M)) {
-	c.Lock()
-	f(c.D[k])
-	c.Unlock()
-}
-
-// Delete 删除
-func (c *Cache[K, M]) Delete(k K) {
-	c.Lock()
-	delete(c.D, k)
-	c.Unlock()
-}
-
-// BatchDelete 批量删除
-func (c *Cache[K, M]) BatchDelete(k []K) {
-	c.Lock()
+// BatchDel 批量删除
+func (c *Cache[K, M]) BatchDel(k []K) {
+	c.D.Lock()
 	for i := 0; i < len(k); i++ {
-		delete(c.D, k[i])
+		delete(c.D.D, k[i])
 	}
-	c.Unlock()
+	c.D.Unlock()
 }
 
-// DeleteBy 删除 f 返回 true 的数据，然后返回
-func (c *Cache[K, M]) DeleteBy(f func(m M) bool) {
-	c.Lock()
-	for k, v := range c.D {
+// DelBy 删除符合 f 的第一个返回
+func (c *Cache[K, M]) DelBy(f func(m M) bool) {
+	c.D.Lock()
+	defer c.D.Unlock()
+	for k, v := range c.D.D {
 		if f(v) {
-			delete(c.D, k)
+			delete(c.D.D, k)
 			break
 		}
 	}
-	c.Unlock()
 }
 
-// BatchDeleteBy 循环删除 f 返回 true 的数据
-func (c *Cache[K, M]) BatchDeleteBy(f func(m M) bool) {
-	c.Lock()
-	for k, v := range c.D {
+// BatchDeleteBy 删除所有符合 f 的
+func (c *Cache[K, M]) BatchDelBy(f func(m M) bool) {
+	c.D.Lock()
+	defer c.D.Unlock()
+	for k, v := range c.D.D {
 		if f(v) {
-			delete(c.D, k)
+			delete(c.D.D, k)
 		}
 	}
-	c.Unlock()
-}
-
-// GetKeys 返回所有 key
-func (c *Cache[K, M]) GetKeys(ctx context.Context) (ks []K, err error) {
-	err = c.Query(ctx, func(c *Cache[K, M]) {
-		for k := range c.D {
-			ks = append(ks, k)
-		}
-	})
-	return
 }
 
 // Get 查询
 func (c *Cache[K, M]) Get(ctx context.Context, k K) (m M, err error) {
-	err = c.Query(ctx, func(c *Cache[K, M]) {
-		m = c.D[k]
-	})
-	return
+	c.D.Lock()
+	defer c.D.Unlock()
+	if !c.ok {
+		if err = c.Load(ctx, c); err != nil {
+			return
+		}
+		c.ok = true
+	}
+	return c.D.D[k], nil
 }
 
 // GetIn 查询
 func (c *Cache[K, M]) GetIn(ctx context.Context, k []K) (ms []M, err error) {
-	err = c.Query(ctx, func(c *Cache[K, M]) {
-		for i := 0; i < len(k); i++ {
-			m, o := c.D[k[i]]
-			if o {
-				ms = append(ms, m)
-			}
+	c.D.Lock()
+	defer c.D.Unlock()
+	if !c.ok {
+		if err = c.Load(ctx, c); err != nil {
+			return
 		}
-	})
+		c.ok = true
+	}
+	for i := 0; i < len(k); i++ {
+		m, o := c.D.D[k[i]]
+		if o {
+			ms = append(ms, m)
+		}
+	}
 	return
 }
 
 // Count 符合 f 的数量
-func (c *Cache[K, M]) Count(ctx context.Context, f func(M) bool) (m int64, err error) {
-	err = c.Query(ctx, func(c *Cache[K, M]) {
-		for _, v := range c.D {
-			if f(v) {
-				m++
-			}
+func (c *Cache[K, M]) Count(ctx context.Context, f func(M) bool) (n int64, err error) {
+	c.D.Lock()
+	defer c.D.Unlock()
+	if !c.ok {
+		if err = c.Load(ctx, c); err != nil {
+			return
 		}
-	})
+		c.ok = true
+	}
+	for _, v := range c.D.D {
+		if f(v) {
+			n++
+		}
+	}
 	return
 }
 
 // Total 总数
-func (c *Cache[K, M]) Total(ctx context.Context, k K) (m int64, err error) {
-	err = c.Query(ctx, func(c *Cache[K, M]) {
-		m = int64(len(c.D))
-	})
+func (c *Cache[K, M]) Total(ctx context.Context, k K) (n int64, err error) {
+	c.D.Lock()
+	defer c.D.Unlock()
+	if !c.ok {
+		if err = c.Load(ctx, c); err != nil {
+			return
+		}
+		c.ok = true
+	}
+	n = int64(len(c.D.D))
 	return
 }
 
 // Foreach 遍历
-func (c *Cache[K, M]) Foreach(ctx context.Context, f func(M)) error {
-	return c.Query(ctx, func(c *Cache[K, M]) {
-		for _, v := range c.D {
-			f(v)
+func (c *Cache[K, M]) Foreach(ctx context.Context, f func(M)) (err error) {
+	c.D.Lock()
+	defer c.D.Unlock()
+	if !c.ok {
+		if err = c.Load(ctx, c); err != nil {
+			return
 		}
-	})
+		c.ok = true
+	}
+	for _, v := range c.D.D {
+		f(v)
+	}
+	return nil
 }
 
-// Search 遍历查询
-func (c *Cache[K, M]) Search(ctx context.Context, f func(M) bool) (ms []M, err error) {
-	err = c.Query(ctx, func(c *Cache[K, M]) {
-		for _, v := range c.D {
-			if f(v) {
-				ms = append(ms, v)
-			}
+// Search 遍历查询符合 f 的第一个返回
+func (c *Cache[K, M]) Search(ctx context.Context, f func(M) bool) (m M, err error) {
+	c.D.Lock()
+	defer c.D.Unlock()
+	if !c.ok {
+		if err = c.Load(ctx, c); err != nil {
+			return
 		}
-	})
+		c.ok = true
+	}
+	for _, v := range c.D.D {
+		if f(v) {
+			m = v
+			break
+		}
+	}
 	return
 }
 
-// SearchOne 遍历查询，f 返回 true 就返回
-func (c *Cache[K, M]) SearchOne(ctx context.Context, f func(M) bool) (m M, err error) {
-	err = c.Query(ctx, func(c *Cache[K, M]) {
-		for _, v := range c.D {
-			if f(v) {
-				m = v
-				return
-			}
+// BatchSearch 遍历查询所有符合 f 的
+func (c *Cache[K, M]) BatchSearch(ctx context.Context, f func(M) bool) (ms []M, err error) {
+	c.D.Lock()
+	defer c.D.Unlock()
+	if !c.ok {
+		if err = c.Load(ctx, c); err != nil {
+			return
 		}
-	})
+		c.ok = true
+	}
+	for _, v := range c.D.D {
+		if f(v) {
+			ms = append(ms, v)
+		}
+	}
 	return
 }
 
-// SearchCache 遍历查询，返回其他的类型
-func SearchCache[T any, K comparable, M any](ctx context.Context, c *Cache[K, M], f func(M) (bool, T)) (ms []T, err error) {
-	err = c.Query(ctx, func(c *Cache[K, M]) {
-		for _, v := range c.D {
-			o, m := f(v)
-			if o {
-				ms = append(ms, m)
-			}
+// SearchCache 遍历查询符合 f 的第一个返回
+func SearchCache[T any, K comparable, M any](ctx context.Context, c *Cache[K, M], f func(M) (bool, T)) (m T, err error) {
+	c.D.Lock()
+	defer c.D.Unlock()
+	if !c.ok {
+		if err = c.Load(ctx, c); err != nil {
+			return
 		}
-	})
+		c.ok = true
+	}
+	for _, v := range c.D.D {
+		o, t := f(v)
+		if o {
+			m = t
+			break
+		}
+	}
+	return
+}
+
+// BatchSearchCache 遍历查询所有符合 f 的
+func BatchSearchCache[T any, K comparable, M any](ctx context.Context, c *Cache[K, M], f func(M) (bool, T)) (ms []T, err error) {
+	c.D.Lock()
+	defer c.D.Unlock()
+	if !c.ok {
+		if err = c.Load(ctx, c); err != nil {
+			return
+		}
+		c.ok = true
+	}
+	for _, v := range c.D.D {
+		o, t := f(v)
+		if o {
+			ms = append(ms, t)
+		}
+	}
 	return
 }
