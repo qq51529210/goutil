43168af5fe645eb79882c034e09aee566535793b
LinWenBin <51529210@qq.com>
2024-02-22 10:50:16 +0800

    u

--- gb28181/util/gb.go
+++ gb28181/util/gb.go
@@ -1,18 +1,22 @@
 package util
 
 import (
-	"gbs/validate"
 	"time"
+)
+
+const (
+	// GBTimeForamt 国标时间格式
+	GBTimeForamt = "2006-01-02T15:04:05"
 )
 
 // GBTime 返回国标格式的时间字符串
 func GBTime() string {
-	return time.Now().Format(validate.GBTimeForamt)
+	return time.Now().Format(GBTimeForamt)
 }
 
 // GBTimestamp 解析并返回时间戳
 func GBTimestamp(t string) int64 {
-	_t, err := time.ParseInLocation(validate.GBTimeForamt, t, time.Local)
+	_t, err := time.ParseInLocation(GBTimeForamt, t, time.Local)
 	if err != nil {
 		return 0
 	}
