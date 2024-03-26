7d6d959daf26d8be6cff4867e1220f687e0ea5e1
LinWenBin <51529210@qq.com>
2024-03-26 10:29:31 +0800

    u

--- sync/lock.go
+++ sync/lock.go
@@ -11,19 +11,21 @@ type Locker struct {
 	// 日志
 	Trace  string
 	Logger *log.Logger
+	// 抢锁周期
+	Interval time.Duration
+	// 抢锁
+	Lock func() (bool, error)
+	// 时间到了，是否启动协程回调 Handle
+	IsTimeup func(d time.Duration) bool
+	// 启用/禁用，协程还是在跑，只是不会回调 Handle
+	IsEnabled func() bool
+	// 在协程中回调
+	Handle func()
 	// 是否抢到
 	locked bool
 	// 并发控制
 	time    time.Time
 	handing int32
-	// 抢锁
-	LockFunc func() (bool, error)
-	// 时间到了，是否启动协程回调 HandleFunc
-	TimeupFunc func(d time.Duration) bool
-	// 启用/禁用，协程还是在跑，只是不会回调 HandleFunc
-	EnableFunc func() bool
-	// 在协程中回调
-	HandleFunc func()
 }
 
 // IsLocked 是否获得锁
@@ -49,35 +51,41 @@ func (l *Locker) routine() {
 	timer := time.NewTimer(0)
 	for {
 		now := <-timer.C
-		if l.EnableFunc() {
-			// 抢锁
-			ok, err := l.LockFunc()
-			if err != nil {
-				l.Logger.ErrorTrace(l.Trace, err)
-			} else {
-				l.locked = ok
-			}
-			// 抢到
-			if l.locked && l.TimeupFunc(now.Sub(l.time)) && atomic.CompareAndSwapInt32(&l.handing, 0, 1) {
-				l.time = now
-				go l.handleRoutine()
-			}
+		// 处理
+		l.handle(&now)
+		// 休息
+		if l.Interval > 0 {
+			timer.Reset(l.Interval)
 		}
-		// 重置计时器
-		timer.Reset(time.Second)
+	}
+}
+
+func (l *Locker) handle(now *time.Time) {
+	// 禁用
+	if !l.IsEnabled() {
+		return
+	}
+	// 抢锁
+	var err error
+	l.locked, err = l.Lock()
+	if err != nil {
+		l.Logger.ErrorTrace(l.Trace, err)
+		return
+	}
+	// 抢到
+	if l.locked && l.IsTimeup(now.Sub(l.time)) && atomic.CompareAndSwapInt32(&l.handing, 0, 1) {
+		l.time = *now
+		go l.handleRoutine()
 	}
 }
 
 // lockRoutine 处理
 func (l *Locker) handleRoutine() {
-	old := time.Now()
 	defer func() {
 		// 异常
 		l.Logger.Recover(recover())
-		// 日志
-		l.Logger.DebugfTrace(l.Trace, "[%v] done", time.Since(old))
 		// 标记
 		atomic.StoreInt32(&l.handing, 0)
 	}()
-	l.HandleFunc()
+	l.Handle()
 }
