10704a518f705848d7b664a85d75b8c52d3b7994
LinWenBin <51529210@qq.com>
2024-04-07 11:13:41 +0800

    u

--- http/http.go
+++ http/http.go
@@ -5,7 +5,7 @@ import "fmt"
 // Result 表示返回的结果
 type Result[T any] struct {
 	// 状态码
-	Code int `json:"code"`
+	Code int `json:"code,omitempty"`
 	// 正确时返回的数据
 	Data T `json:"data,omitempty"`
 	// 错误短语
