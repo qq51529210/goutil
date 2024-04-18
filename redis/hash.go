956ba22f8ac3ac65cd32691caa3587daedb69016
LinWenBin <51529210@qq.com>
2024-04-18 11:32:54 +0800

    u

--- redis/hash.go
+++ redis/hash.go
@@ -7,37 +7,18 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// HSetNxEx 存在才设置
-func HSetNxEx(ctx context.Context, db redis.UniversalClient, key string, expire time.Duration, field string, value any) (bool, error) {
-	res, err := redis.NewScript(`
-local result = redis.call("EXISTS", KEYS[1])
--- 存在
-if result == 1 then
-	-- 更新
-	redis.call("HSET", KEYS[1], ARGV[1], ARGV[2])
-	redis.call("EXPIRE", KEYS[1], ARGV[3])
-end
--- 返回
-return result
-`).Run(ctx, db, []string{key}, field, value, expire/time.Second).Result()
-	if err != nil {
-		return false, err
-	}
-	return res.(int64) == 1, nil
-}
-
 // HSetNx 存在才设置
-func HSetNx(ctx context.Context, db redis.UniversalClient, key, field string, value any) (bool, error) {
+func HSetNx(ctx context.Context, db redis.UniversalClient, key string, value ...any) (bool, error) {
+	//
 	res, err := redis.NewScript(`
 local result = redis.call("EXISTS", KEYS[1])
 -- 存在
 if result == 1 then
 	-- 更新
-	redis.call("HSET", KEYS[1], ARGV[1], ARGV[2])
+	redis.call("HMSET", KEYS[1], unpack(ARGV))
 end
 -- 返回
-return result
-`).Run(ctx, db, []string{key}, field, value).Result()
+return result`).Run(ctx, db, []string{key}, value...).Result()
 	if err != nil {
 		return false, err
 	}
@@ -45,22 +26,21 @@ return result
 }
 
 // HMSetNxEx 存在才设置
-func HMSetNxEx(ctx context.Context, db redis.UniversalClient, key string, expire time.Duration, args ...any) (bool, error) {
-	var values []any
-	values = append(values, expire/time.Second)
-	values = append(values, args...)
+func HSetNxEx(ctx context.Context, db redis.UniversalClient, key string, expire time.Duration, values ...any) (bool, error) {
+	var args []any
+	args = append(args, expire/time.Second)
+	args = append(args, values...)
 	//
 	res, err := redis.NewScript(`
 local result = redis.call("EXISTS", KEYS[1])
 -- 存在
 if result == 1 then
 	-- 更新
-	redis.call("HSET", KEYS[1], ARGV[2], ARGV[3])
+	redis.call("HMSET", KEYS[1], unpack(ARGV, 2))
 	redis.call("EXPIRE", KEYS[1], ARGV[1])
 end
 -- 返回
-return result
-`).Run(ctx, db, []string{key}, values...).Result()
+return result`).Run(ctx, db, []string{key}, args...).Result()
 	if err != nil {
 		return false, err
 	}
