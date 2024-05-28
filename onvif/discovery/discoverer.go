dd0c687f7f29d71ae7e6d769abb76c1fe9722c20
LinWenBin <51529210@qq.com>
2024-05-28 15:58:02 +0800

    u

--- onvif/discovery/discoverer.go
+++ onvif/discovery/discoverer.go
@@ -10,6 +10,42 @@ import (
 	"time"
 )
 
+const (
+	// 用于读取消息的大小，10k 足够了
+	readBufLen = 1024 * 10
+	// MulticastAddr 默认的使用的多播地址
+	MulticastAddr = "239.255.255.250:3702"
+)
+
+const (
+	// 发送的消息，就差 uuid 了
+	msgFmt = `<?xml version="1.0" encoding="UTF-8"?>
+<Envelope xmlns="http://www.w3.org/2003/05/soap-envelope" xmlns:tds="http://www.onvif.org/ver10/device/wsdl">
+  <Header xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing">
+    <a:MessageID>uuid:%s</a:MessageID>
+    <a:To>urn:schemas-xmlsoap-org:ws:2005:04:discovery</a:To>
+    <a:Action>http://schemas.xmlsoap.org/ws/2005/04/discovery/Probe</a:Action>
+  </Header>
+  <Body>
+    <Probe xmlns="http://schemas.xmlsoap.org/ws/2005/04/discovery" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+		<Types>tds:Device</Types>
+    </Probe>
+  </Body>
+</Envelope>`
+)
+
+// result 用于解析地址
+type result struct {
+	XMLName xml.Name `xml:"Envelope"`
+	Body    struct {
+		ProbeMatches struct {
+			ProbeMatch struct {
+				XAddrs string
+			}
+		}
+	}
+}
+
 // Callback 回调，err 是 net.Conn 返回的错误，超时是正常
 type Callback func(addr string, err error)
 
